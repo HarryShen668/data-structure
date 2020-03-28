1ff1f3ce52b15608206aecd0ce33081f289fa7fb
shy-shengxin <60008745+shy-shengxin@users.noreply.github.com>
2020-03-28 16:59:52 +0800

    加入顺序表与注释

--- list.cpp
+++ list.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include"linklist.h"
+#include"linelist.h"
 int main()
 {
 	int i;
@@ -15,24 +16,87 @@ start:
 		scanf_s("%d", &i);
 		system("cls");
 		int flag = 1;
-
-
+		int flag2 = 1;
 		switch (i)
 		{
-		case 2:
+		case 1://顺序表
+			Sqlist L;
+			do
+			{
+				printf("顺序表的二级菜单为：\n");
+				printf("1.创建顺序线性表:\n");
+				printf("2.求线性顺序表的长度\n");
+				printf("3.按序号取线性顺序表的元素\n");
+				printf("4.按值查找线性顺序表\n");
+				printf("5.插入元素\n");
+				printf("6.删除元素\n");
+				printf("7.输出顺序线性表\n");
+				printf("8.返回\n");
+				int g;
+				scanf_s("%d", &g);
+				system("cls");
+				switch (g)
+				{
+				case 1:
+					Creatlist(&L);
+					break;//创建顺序线性表
+
+				case 2:
+					getlen(&L);
+					break;//求线性顺序表的长度
+				case 3:
+					int n;
+					int t;
+					scanf_s("%d", &n);
+					t = getelem( &L,n);
+					break;//按序号取线性表的元素
+				case 4:
+					ElemType e3;
+					printf("now cin the Elemtype\n");
+					scanf_s("%d", &e3);
+					Locate(&L, e3);
+					break;//按值查找线性表
+				case 5:
+					int pos;
+					ElemType e;
+					printf("please cin the pos\n");
+					scanf_s("%d", &pos);
+					printf("now cin the Elemtype\n");
+					scanf_s("%d", &e);
+					Insert( &L, pos, e);
+					break;//插入元素
+				case 6:
+					int pos1, k;
+					ElemType e4;
+					printf("please cin the pos\n");
+					scanf_s("%d", &pos1);
+					k = linedelete(&L, pos1,&e4);
+					if (k == 0)
+						printf("successful delete\n ");
+					break;//删除元素
+				case 7:
+					showlinelist( & L);
+					break;//输出顺序线性表
+				case 8:
+					flag2 = 0;
+					break;//返回
+				}
+			} while (flag2);
+			break;
+		case 2://链表
 			Link headlink = (LNode*)malloc(sizeof(LNode));
 			headlink->next = NULL;
 			do
 			{
 
-				printf("顺序表的二级菜单为：\n");
-				printf("1.创建顺序线性表:\n");
-				printf("2.求线性顺序表的长度\n");
-				printf("3.按序号取线性表的元素\n");
-				printf("4.按值查找线性表、\n");
+				printf("链表的二级菜单为：\n");
+				printf("1.创建线性链表:\n");
+				printf("2.求线性链表的长度\n");
+				printf("3.按序号取线性链表的元素\n");
+				printf("4.按值查找线性链表、\n");
 				printf("5.插入元素\n");
 				printf("6.删除元素\n");
-				printf("7.输出顺序线性表\n");
+				printf("7.输出线性链表\n");
 				printf("8.返回\n");
 				int y;
 				scanf_s("%d", &y);
@@ -44,26 +108,26 @@ start:
 					int n;
 					scanf_s("%d", &n);
 					create(headlink, n);
-					break;
+					break;//创建线性链表
 
 				case 2:
 					int num;
 					num = Getlen(headlink);
 					printf("%d\n", num);
-					break;
+					break;//求线性链表的长度
 				case 3:
 					int node;
 					Link t;
 					scanf_s("%d", &node);
 					t = Getnode(headlink, node);
 					printf("%d\n", t->data);
-					break;
+					break;//按序号取线性链表的元素
 				case 4:
 					ElemType e1;
 					printf("now cin the Elemtype\n");
 					scanf_s("%d", &e1);
 					Getnum(headlink, e1);
-					break;
+					break;//按值查找线性链表
 				case 5:
 					int pos;
 					ElemType e;
@@ -72,7 +136,7 @@ start:
 					printf("now cin the Elemtype\n");
 					scanf_s("%d", &e);
 					insert(headlink, pos, e);
-					break;
+					break;//插入元素
 				case 6:
 					int pos1, k;
 					printf("please cin the pos\n");
@@ -80,17 +144,18 @@ start:
 					k = Delete(headlink, pos1);
 					if (k == 0)
 						printf("successful delete\n ");
-					break;
+					break;//删除元素
 				case 7:
 					showlist(headlink);
-					break;
+					break;//输出线性链表
 				case 8:
 					flag = 0;
-					break;
+					break;//返回
 				}
 			} while (flag);
 			break;
-		}
+
+		}		
 	} while (flag1);
 	return 0;
 }
